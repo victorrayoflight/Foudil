10d2a6c46559dd1f84da2a0424b4c650dfc75cb1
Victor Medvedev <33pass@gmail.com>
2019-02-19 16:04:46 -0500

    Update

--- Examen 2019-02-18/16.py
+++ Examen 2019-02-18/16.py
@@ -1,14 +1,15 @@
 user_list = []
+user_list_count = 0
 
 number_of_elements = int(input("Entrez le nombre d'entiers : "))
 
-for count in range(number_of_elements):
+while user_list_count != number_of_elements:
     try:
         element = int(input("Entrez un element :"))
+        user_list.append(element)
+        user_list_count += 1
     except ValueError:
         print("Ce n'est pas un nombre entier. Essayez encore.")
-        continue
-    user_list.append(element)
 
 print("La liste creé :", user_list)
 
@@ -21,20 +22,30 @@ print("La liste creé :", user_list)
 
 
 
-
-user_list = []
-
-number_of_elements = int(input("Entrez le nombre d'entiers : "))
-
-for count in range(number_of_elements):
-    element = input("Entrez un element :")
-    element_type = type(element)
-    if element_type != 'int':
-        continue
-    if_int = element.isdigit
-    except ValueError:
-        print("Ce n'est pas un nombre entier. Essayez encore.")
-        continue
-    user_list.append(element)
-
-print("La liste creé :", user_list)
+# for count in range(number_of_elements):
+#     try:
+#         element = int(input("Entrez un element :"))
+#     except ValueError:
+#         print("Ce n'est pas un nombre entier. Essayez encore.")
+#         continue
+#     user_list.append(element)
+#
+#
+#
+#
+# user_list = []
+#
+# number_of_elements = int(input("Entrez le nombre d'entiers : "))
+#
+# for count in range(number_of_elements):
+#     element = input("Entrez un element :")
+#     element_type = type(element)
+#     if element_type != 'int':
+#         continue
+#     if_int = element.isdigit
+#     except ValueError:
+#         print("Ce n'est pas un nombre entier. Essayez encore.")
+#         continue
+#     user_list.append(element)
+#
+# print("La liste creé :", user_list)
--- Examen 2019-02-18/6.py
+++ Examen 2019-02-18/6.py
@@ -2,11 +2,29 @@ x = True
 y = False
 z = False
 
+# False OR True == True
+# True OR False == True
+# True OR True == True
+# False OR False == False
+
+# True AND True = True
+# True AND False = False
+# False AND True = False
+# False AND False = False
+
+# False XOR True == True
+# True XOR False == True
+# True XOR True == False
+# False XOR False == False
+
 if not x or y:
+    # False or False == False
     print(1)
 elif not x or not y and z:
+    # False or True and False == False
     print(2)
 elif not x or y or not y and x:
+    # False or False or True and True == True
     print(3)
 else:
     print(4)
