0003cde1c8e38d8f63a78f1c1f4e04b0be96d910
longgone <33pass@gmail.com>
2019-03-14 18:31:00 -0400

    07
    # what is a value of k at the end of script? 2 vedi?

--- Examen 2019-03-14/07.py
+++ Examen 2019-03-14/07.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+def nPrint(message, n):
+    while n > 0:
+        print(message)
+        n -= 1
+
+k = 2
+
+nPrint("Salut tout le monde !", k)
+
+
+# what is a value of k at the end of script? 2 vedi?
