8c6cd53cfca6a3f41b15fff894edd0120d1fb81b
longgone <33pass@gmail.com>
2019-03-14 18:20:36 -0400

    08
    t1 = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
    t2 = ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre']
    
    sovmestit eti 2 lists
    
    doljno vyvesti
    
    'Janvier', '31', 'Fevrier', '28' etc

--- Examen 2019-03-14/08.py
+++ Examen 2019-03-14/08.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+
+t1 = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+t2 = ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre']
+
+sovmestit eti 2 lists
+
+doljno vyvesti
+
+'Janvier', '31', 'Fevrier', '28' etc
